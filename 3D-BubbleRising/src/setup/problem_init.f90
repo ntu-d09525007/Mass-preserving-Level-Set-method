0988b433b7b1d03a4fa3fc43e26cdd79d38837c9
leo <luisleo526@yahoo.com>
2023-04-27 06:26:42 +0800

    debug

--- 3D-BubbleRising/src/setup/problem_init.f90
+++ 3D-BubbleRising/src/setup/problem_init.f90
@@ -70,6 +70,8 @@ CHARACTER(100) :: NAME_OF_FILE
                                                  & -sqrt((x-2.0)**2+(y-2.0)**2+(z-2.5)**2) + 0.5)
             endif
             
+            p%of(id)%loc%phi%now(i,j,k) = - p%of(id)%loc%phi%now(i,j,k)
+
         end do
         end do
         end do
