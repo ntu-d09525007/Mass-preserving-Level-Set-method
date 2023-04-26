29501f3ebc9d3b85696c096ea850bddb85b5e4ce
leo <luisleo526@yahoo.com>
2023-04-27 06:21:38 +0800

    debug

--- 3D-BubbleRising/lib/data/tree.f90
+++ 3D-BubbleRising/lib/data/tree.f90
@@ -369,6 +369,10 @@ real(8) :: kh,ap
         call p%mg_setup
         write(*,*)"finish initializing multigrid exact solver"
     endif
+
+    if( p%glb%inverse )then
+        p%glb%btn_sf = - p%glb%btn_sf
+    endif
     
 end subroutine
 
