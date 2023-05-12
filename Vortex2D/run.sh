c11ea850f05b81cf5a7aa5fd6588b69cd7f3a6f0
leo <luisleo526@yahoo.com>
2023-05-13 00:55:31 +0800

    debug bash file

--- Vortex2D/run.sh
+++ Vortex2D/run.sh
@@ -4,10 +4,11 @@ rm -rf Vortex_Results.txt
 
 for r in 1.0 0.1 0.01 0.001 
 do
-	sed -i -e "50 c $r\t0.01" default.txt
+	sed -i -e "50 c $r\t0.01" input/default.txt
 	for dx in 32 64 128 256
 	do
-		sed -i -e "14 c $dx" default.txt
+		sed -i -e "4 c Vortex_$r_$dx" input/default.txt
+		sed -i -e "14 c $dx" input/default.txt
 		START=$(date +%s%N)
 		./RUN
 		END=$(date +%s%N)
